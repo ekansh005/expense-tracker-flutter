c0b9b6bbf2072372bc5e3f1f672c2b88b0634a14
Ekansh Saxena <ekansh.saxena@cerion.fi>
2020-06-04 13:08:06 +0300

    extracted new widget for transaction item

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,5 +1,5 @@
+import 'package:expense_tracker/widgets/transaction_item.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
@@ -31,39 +31,10 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text('\$${transactions[index].amount}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      '${transactions[index].title}',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMMd().format(transactions[index].date),
-                    ),
-                    trailing: mediaQuery.size.width > 460
-                        ? FlatButton.icon(
-                            onPressed: () => deleteTxn(transactions[index].id),
-                            icon: Icon(Icons.delete),
-                            label: Text('Delete'),
-                            textColor: Theme.of(context).errorColor,
-                          )
-                        : IconButton(
-                            icon: Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                            onPressed: () => deleteTxn(transactions[index].id),
-                          ),
-                  ),
-                );
+                return TransactionItem(
+                    transaction: transactions[index],
+                    mediaQuery: mediaQuery,
+                    deleteTxn: deleteTxn);
               },
               itemCount: transactions.length,
             ),
